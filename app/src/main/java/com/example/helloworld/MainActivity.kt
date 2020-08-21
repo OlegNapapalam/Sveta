9e6a3a9f3cde0ba4ea36675010423c4405fc6497
iNapalm <apoloninoleg@gmail.com>
2020-08-21 20:34:04 +0300

    lights
    светофор ++

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+
+    var makeRed: LinearLayout? = null
+    var makeGreen: LinearLayout? = null
+    var makeYellow: LinearLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        makeRed = findViewById(R.id.Red)
+        makeGreen = findViewById(R.id.Green)
+        makeYellow = findViewById(R.id.Yellow)
+    }
+
+    fun buttonRedClick(view: View) {
+        makeRed?.setBackgroundResource(R.drawable.ic_sem_red)
+        makeGreen?.setBackgroundResource(R.drawable.ic_sem_black)
+        makeYellow?.setBackgroundResource(R.drawable.ic_sem_black)
+    }
+    fun buttonYellowClick(view: View) {
+        makeRed?.setBackgroundResource(R.drawable.ic_sem_black)
+        makeGreen?.setBackgroundResource(R.drawable.ic_sem_yellow)
+        makeYellow?.setBackgroundResource(R.drawable.ic_sem_black)
+    }
+    fun buttonGreenClick(view: View) {
+        makeRed?.setBackgroundResource(R.drawable.ic_sem_black)
+        makeGreen?.setBackgroundResource(R.drawable.ic_sem_black)
+        makeYellow?.setBackgroundResource(R.drawable.ic_sem_green)
     }
 }
